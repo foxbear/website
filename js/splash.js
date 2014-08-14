c279e803155821afd461c6ce5e1a6d1f72a62242
John Victor-Faichney <john.victor-faichney@aditazz.com>
2014-08-13 22:07:56 -0700

    added placeholder fading animation

--- js/splash.js
+++ js/splash.js
@@ -1,13 +1,14 @@
 var canvas, words, fox, bear;
 paper.install(window);
 
+// Import SVG tag to the canvas, re-scale and assign event handlers
 window.onload = function() {
 		// Get a reference to the canvas object
 		canvas = document.getElementById('splashCanvas');
 		// Create an empty project and a view for the canvas:
 		paper.setup(canvas);
 		// Create a Paper.js Path to draw a line into it:
-		words = project.importSVG(document.getElementById('svg'));
+		words = project.importSVG(document.getElementById('svg') , { expandShapes: true } );
 		// Do some initial scaling/positioning
         words.scale(1, .8); // correct initial scaling from SVG import
 		updateScale( .5 );
@@ -19,7 +20,40 @@ window.onload = function() {
 		view.onResize = function(event) {
 			updateScale( .5 );
 		}
+        
+        // Whenever a frame occurs, updateFrame
+        view.onFrame = function( event ){
+            updateFrame( event , fox, bear );
+        }
+}
 
+// For now, oscillate opacity. Eventually , morph words...
+updateFrame = function( event , fox, bear ){
+
+    var period = 480
+    var frame_count = event.count
+    // Create a periodic function that counts down to zero from some value,
+    // then back up to the value,
+    var i = Math.abs(frame_count%period - ( period/2 ) );
+    // console.log(i);
+    
+    var fox_opac = map_range( i , 0 , ( period/2 ) , 0 , 1)
+    var bear_opac = map_range( i , 0 , ( period/2 ) , 1 , 0)
+    
+    fox.children.forEach( function(letter_group){
+        if ( letter_group.name != "blank" ){
+            letter_path = letter_group.children[0]
+            // letter_path.strokeColor.alpha = fox_opac;
+            letter_path.fillColor.alpha = fox_opac;
+        }
+    });
+    
+    bear.children.forEach( function(letter_group){
+        letter_path = letter_group.children[0];
+        letter_path.strokeColor.alpha = bear_opac;
+        // letter_path.fillColor.alpha = bear_opac;
+    }); 
+    
 }
 
 // Scale 'words' to the new window dimension, then reset the desired scale
@@ -30,40 +64,6 @@ updateScale = function( scale ){
 	words.scale( scale );
 }
 
-/*
-words.fillColor = null;
-words.strokeColor = 'black';
-
-var yesGroup = words.children.yes;
-var noGroup = words.children.no;
-yesGroup.transformContent = true;
-noGroup.transformContent = true;
-
-// Resize the words to fit snugly inside the view:
-project.activeLayer.fitBounds(view.bounds);
-project.activeLayer.scale(0.8);
-
-yesGroup.position = view.center;
-noGroup.position = [-900, -900];
-
-function onMouseMove(event) {
-	noGroup.position = event.point;
-	for (var i = 0; i < yesGroup.children.length; i++) {
-		for (var j = 0; j < noGroup.children.length; j++) {
-			showIntersections(noGroup.children[j], yesGroup.children[i])
-		}
-	}
+map_range = function (value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
-
-function showIntersections(path1, path2) {
-	var intersections = path1.getIntersections(path2);
-	for (var i = 0; i < intersections.length; i++) {
-		new Path.Circle({
-			center: intersections[i].point,
-			radius: 5,
-			fillColor: '#009dec'
-		}).removeOnMove();
-	}
-}
-*/
-
